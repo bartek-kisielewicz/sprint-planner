3b53246b3a5381f3a08164b0b5f1f29dce92952f
Bartosz Kisielewicz <bartosz.kisielewicz@imgarena.com>
2020-12-14 13:05:25 +0000

    mock db and styled-components

---
+++ src/mockedDB/issues.js
@@ -0,0 +1,10 @@
+export const issues = [
+  {
+    id: 1,
+    title: 'Issue 1',
+    description: 'As bookmaker website user I want see Fight Action Feed in ufc event centre, so that I can be more confident about the bet I make',
+    owner: "Johan Lajili",
+    reviewers: ["Bartosz Kisielewicz", "Renata Svaboviciute"],
+    storyPoints: 8
+  },
+];
